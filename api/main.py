3466572bbac9c56e718f95c780dd7b7ebd246cc6
danelsender <de296@exeter.ac.uk>
2023-02-17 15:33:12 +0000

    added homepage and hello_name in api
    Files changed:
    
    main.py

--- api/main.py
+++ api/main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
+import datetime
 
 app = FastAPI()
+
+@app.get("/")
+async def homepage():
+    return f"Hello, World!"
+
+@app.get("/hello/{name}")
+async def hello_name(name: str):
+    return f"Hello, {name}!"
