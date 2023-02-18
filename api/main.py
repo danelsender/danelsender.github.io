41fa5f7e0ab8f51d4f16380bae3ad027a527dee3
danelsender <de296@exeter.ac.uk>
2023-02-18 14:08:16 +0000

    added top nav bar, with code, cv, photography pages.
    files modified:
    main.py
    base.html
    
    files created:
    code.html
    cv.html
    gallery.html
    publications.html

--- api/main.py
+++ api/main.py
@@ -14,3 +14,19 @@ async def homepage(request: Request):
 @app.get("/hello/{name}")
 async def hello_name(name: str):
     return f"Hello, {name}!"
+
+@app.get("/code", response_class=HTMLResponse)
+async def homepage(request: Request):
+    return settings.TEMPLATES.TemplateResponse("code.html", {"request": request})
+
+@app.get("/cv", response_class=HTMLResponse)
+async def homepage(request: Request):
+    return settings.TEMPLATES.TemplateResponse("cv.html", {"request": request})
+
+@app.get("/gallery", response_class=HTMLResponse)
+async def homepage(request: Request):
+    return settings.TEMPLATES.TemplateResponse("gallery.html", {"request": request})
+
+@app.get("/publications", response_class=HTMLResponse)
+async def homepage(request: Request):
+    return settings.TEMPLATES.TemplateResponse("publications.html", {"request": request})
